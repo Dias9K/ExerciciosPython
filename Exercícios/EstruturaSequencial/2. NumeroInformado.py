ed070f4a18d61ac5dacdbe2c74b026f688f6a151
Gabriel Dias <gabrielmdais557@gmail.com>
2024-02-21 00:14:22 -0300

    cabeçalhos

--- Exercícios/EstruturaSequencial/2. NumeroInformado.py
+++ Exercícios/EstruturaSequencial/2. NumeroInformado.py
@@ -1,2 +1,4 @@
-numero = input("Insira um número: ")
+## Faça um Programa que peça um número e então mostre a mensagem "O número informado foi [número]".
+
+numero = input("Insira um número: ")  ## input() retorna sempre uma string
 print("O número inserido foi: " + numero)
