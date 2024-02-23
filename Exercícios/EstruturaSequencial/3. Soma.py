96b18270bebd4a06780557e9aa647467fef07cb5
Gabriel Dias <gabrielmdais557@gmail.com>
2024-02-23 06:17:49 -0300

    soma

--- Exercícios/EstruturaSequencial/3. Soma.py
+++ Exercícios/EstruturaSequencial/3. Soma.py
@@ -1,1 +1,7 @@
-## Faça um Programa que peça dois números e imprima a soma.
+## Faça um programa que peça dois números e imprima a soma.
+
+numero1 = int(input("digite o primeiro número: "))
+numero2 = int(input("digite o segundo número: "))
+
+print("A soma dos números digitados é: ", numero1 + numero2)
+## é preciso usar a ",", pois não é possível concatenar uma string e um valor numérico utilizando o "+"
