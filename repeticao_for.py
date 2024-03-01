39cd11daa63d5956e9e43f33521a49c88d8c4153
Gabriel Dias <gabrielmdais557@gmail.com>
2024-03-01 20:34:57 -0300

    upper + 11

---
+++ repeticao_for.py
@@ -0,0 +1,9 @@
+texto = input("Digite o seu texto: ")
+VOGAIS = "AEIOU"
+
+for letra in texto:
+    if letra.upper() in VOGAIS:
+# o método upper retorna uma letra maiúscula.
+# neste contexto, o método está verificando se há uma letra máiuscula
+# que contenha no texto, se sim, ele retorna essa letra que tem no texto 
+        print(letra)
