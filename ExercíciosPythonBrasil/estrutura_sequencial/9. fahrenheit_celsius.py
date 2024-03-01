faad41acf91ae88143f38913ff0ef161ca4f848f
Gabriel Dias <gabrielmdais557@gmail.com>
2024-02-29 23:50:39 -0300

    9. fahrenheit_celsius + q1_estrutura_dados

---
+++ ExercíciosPythonBrasil/estrutura_sequencial/9. fahrenheit_celsius.py
@@ -0,0 +1,8 @@
+# Faça um Programa que peça a temperatura em graus Fahrenheit, transforme e mostre a temperatura em graus Celsius.
+# C = 5 * ((F-32) / 9).
+
+fahrenheit = float(input("Digite a temperatura em graus Fahrenheit: "))
+celsius = (fahrenheit - 32) / 1.8
+
+# %.2f igual em C para formatar um número, mas a representação da variável, é utilizado o % também
+print("A temperatura convertida em graus Celsius é: %.2f" % celsius) 
