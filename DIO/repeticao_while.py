c9208faf49ec68f6cb71cdb870ed5bdcbaf95253
Gabriel Dias <gabrielmdais557@gmail.com>
2024-03-03 19:06:27 -0300

    break_continue

--- DIO/repeticao_while.py
+++ DIO/repeticao_while.py
@@ -1,10 +1,15 @@
+numero = 0
+
 opcao = -1
 while opcao != 0:
     opcao = int(input("[1] Exibir extrato [2] Sacar [0] Sair\n"))
     if opcao == 1:
         print("Exibindo extrato...")
+        # break PARA a execução na linha que está inserido
     elif opcao == 2:
-        print("Sacando...")
+        for numeral in range(10):
+            print(numeral, end=(", "))
+        continue 
+        print("Sacando...") # PARA a sua execução em todas as linhas seguintes após a sua instanciação
 else:
     print("Saindo...")
-# TODO break
