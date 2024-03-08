341ded9a60c15382eefab93de597e79e4e8e735e
Gabriel Dias <gabrielmdais557@gmail.com>
2024-03-08 19:35:46 -0300

    list_ok

--- DIO/listas_copy.py
+++ DIO/listas_copy.py
@@ -1,4 +1,4 @@
-# .copy()
+# [].copy()
 lista1 = [1, "string", [10, 40, 80]]
 lista2 = lista1.copy()  # copia a todos os elementos da lista
 print(
@@ -8,13 +8,48 @@ lista2[0] = 10
 print(lista1)
 print(lista2)
 
-# .count()
+# [].count()
 frutas = ["laranja", "morango", "limão", "laranja", "banana"]
 print(frutas.count("laranja"))  # retorna quantas vezes um elemento se repete na lista
 print(frutas.count("morango"))
 
-# .extend()
+# [].extend()
 linguagens = ["python", "java"]
 print(linguagens)
-linguagens.extend(["js", "c", "c#"])  # acrescenta uma outra lista em uma já existente
+linguagens.extend(
+    ["js", "c", "c#", "java"]
+)  # acrescenta uma outra lista em uma já existente
+print(linguagens)
+# [].sort()
+linguagens.sort()  # ordena os elementos na ordem alfabética
+print(linguagens)
+linguagens.sort(reverse=True)
+print(linguagens)  # ordena os elementos na ordem alfabética descendente
+linguagens.sort(
+    key=lambda x: len(x)
+)  # função anônima que ordena os elementos de acordo com a sua quantidade de caracteres
+print(linguagens)
+linguagens.sort(
+    key=lambda x: len(x), reverse=True
+)  # função anônima que ordena os elementos de acordo com a sua quantidade de caracteres e com a ordem alfabética descendente
+print(linguagens)
+
+
+# [].index()
+print(
+    linguagens.index("java")
+)  # retorna a primeira ocorrência de um elemento específico
+
+# [].pop()
+print(linguagens)
+linguagens.pop()  # retira o último item da lista por padrão, conceito de pilha (LIFO)
+linguagens.pop(0)  # também pode ser utilizado passando o index
+print(linguagens)
+
+# [].remove()
+linguagens.remove("java")  # remove um item específico da lista
+print(linguagens)
+
+# [].reverse()
+linguagens.reverse()  # reverte a ordem dos elementos da lista
 print(linguagens)
