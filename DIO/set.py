3150ceb8314eb0f89e92c3834983be2cf1098e15
Gabriel Dias <gabrielmdais557@gmail.com>
2024-03-09 15:25:27 -0300

    set2

--- DIO/set.py
+++ DIO/set.py
@@ -1,5 +1,16 @@
 """Um set é uma coleção que não possui objetos repetidos, usamos sets para representar conjuntos matemáticos
 ou eliminar itens duplicados de um iterável"""
 
-carros = ["palio", "gol", "celta", "palio"]
-print(set(carros))
+carros = ["palio", "gol", "celta", "palio"]  # declarado em "()", é uma lista
+linguagens = {"python", "java", "python"}  # declarado em "{}", é um conjunto, eles não suportam indexação ou fatiamento
+# e nem é possível, passar o index para acesso. caso queira acessar os valores, é preciso converter o set em lista
+linguagens = list(linguagens)
+
+# retorna os elementos e o seu index
+# for carro in enumerate(carros):
+#     print(f"{carro}")
+
+for indice, carro in enumerate(carros):
+    print(f"{carro}")
+
+# métodos da classe set
