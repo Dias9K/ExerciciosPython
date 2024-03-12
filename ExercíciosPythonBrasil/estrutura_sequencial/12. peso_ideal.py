e6c12bf9aef01e37aa64ad20eaedf1032bed8c80
Gabriel Dias <gabrielmdais557@gmail.com>
2024-03-12 01:50:59 -0300

    12peso_ideal

---
+++ ExercíciosPythonBrasil/estrutura_sequencial/12. peso_ideal.py
@@ -0,0 +1,3 @@
+"""Tendo como dados de entrada a altura de uma pessoa, construa um algoritmo que calcule seu peso ideal,
+usando a seguinte fórmula: (72.7*altura) - 58"""
+peso_atual = float(input("Digite o seu peso atual: "))
