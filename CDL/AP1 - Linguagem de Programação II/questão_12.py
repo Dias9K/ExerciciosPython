5234963938f962a2515931b29525147dee34a233
Gabriel Dias <gabrielmdais557@gmail.com>
2024-03-21 01:17:08 -0300

    add questão_12.py

--- CDL/AP1 - Linguagem de Programação II/questão_12.py
+++ CDL/AP1 - Linguagem de Programação II/questão_12.py
@@ -7,3 +7,12 @@
 # de quilos além do limite e na variável multa o valor da multa que João deverá pagar. Imprima
 # os dados do programa com as mensagens adequadas
 
+peso_peixes = float(input("Digite a quantidade de quilos em peixes que João fisgou em sua pesca diária: "))
+
+if peso_peixes > 50.0:
+    excesso = peso_peixes - 50.0  # atribuído apenas caso ele ultrapasse o limite de 50kg
+    print(f"João ultrapassou {excesso:.2f}kg na sua pesca e foi multado!")
+    valor_multa = 4.0 * excesso
+    print(f"O valor da sua multa é = R${valor_multa:.2f}!")
+else:
+    print("João não deve pagar o valor da multa pois não excedeu o peso da sua pesca!")
