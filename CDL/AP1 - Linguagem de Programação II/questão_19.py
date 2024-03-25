63d09175349fa7ec4dfff5cce50039c953c1a529
Gabriel Dias <gabrielmdais557@gmail.com>
2024-03-24 23:32:28 -0300

    valores e quantidades tintas

--- CDL/AP1 - Linguagem de Programação II/questão_19.py
+++ CDL/AP1 - Linguagem de Programação II/questão_19.py
@@ -1,3 +1,4 @@
+import math
 # Questão 19 – (1 ponto)
 # GABRIEL MAGALHÃES DIAS
 # Faça um Programa para uma loja de tintas. O programa deverá pedir o tamanho em metros
@@ -12,8 +13,23 @@
 # - misturar latas e galões, de forma que o preço seja o menor. Acrescente 10% de folga e
 # sempre arredonde os valores para cima, isto é, considere latas cheias.
 
-lata_18L = 80.00
-galao_3_6L = 25.00
+preco_lata_18L = 80
+litros_lata = 18
+preco_galao_3_6L = 25
+litros_galao = 3.6
+
 print("Color Tintas Store".center(50, "="),
-      "\nNossa loja está com promoções imperdíveis")
+      "\nSeja muito bem vindo, a nossa loja está com promoções imperdíveis para você!!")
 metros_quadrados = float(input("Digite em metros quadrados a área a ser pintada: "))
+
+# calculando as quantidades das tintas
+quantidade_litros = metros_quadrados/6
+# o método math.ceil retornará um valor inteiro arredondado para o próximo número
+quantidade_latas = math.ceil(quantidade_litros / litros_lata)
+quantidade_galoes = math.ceil(quantidade_litros / litros_galao)
+quantidade_mix = math.ceil()
+# TODO calcular a quantidade e valor dos mixes
+# calculando o valor dos casos (só latas, galões ou os dois juntos)
+valor_apenas_latas = preco_lata_18L*quantidade_latas
+valor_apenas_galoes = preco_galao_3_6L*quantidade_galoes
+valor_mix =
