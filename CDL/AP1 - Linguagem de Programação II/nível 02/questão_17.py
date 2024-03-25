56c766f9fbe986d38f58bd455b986c7f04a8e347
Gabriel Dias <gabrielmdais557@gmail.com>
2024-03-25 14:32:01 -0300

    ➕ add (questão_17.py)

---
+++ CDL/AP1 - Linguagem de Programação II/nível 02/questão_17.py
@@ -0,0 +1,16 @@
+import operator
+from functools import reduce
+# Questão 17 – (0,75 pontos)
+# GABRIEL MAGALHÃES DIAS
+# Faça um Programa que leia um vetor de 5 números inteiros, mostre a soma, a multiplicação e
+# os números.
+
+vetor = []
+for i in range(5):
+    inteiros = int(input("Digite um numero inteiro: "))
+# TODO um meio de conversão forçada do float para inteiro
+    vetor.append(inteiros)
+
+print(f"Valores contidos no vetor = {vetor}\n"
+      f"Soma dos valores contidos no vetor = {sum(vetor)}\n"
+      f"Multiplicação dos valores contidos no vetor = {reduce(operator.mul, vetor)}\n")
--- CDL/AP1 - Linguagem de Programação II/nível 03/questão_19.py
+++ CDL/AP1 - Linguagem de Programação II/nível 03/questão_19.py
@@ -1,4 +1,5 @@
 import math
+
 # Questão 19 – (1 ponto)
 # GABRIEL MAGALHÃES DIAS
 # Faça um Programa para uma loja de tintas. O programa deverá pedir o tamanho em metros
@@ -23,13 +24,26 @@ print("Color Tintas Store".center(50, "="),
 metros_quadrados = float(input("Digite em metros quadrados a área a ser pintada: "))
 
 # calculando as quantidades das tintas
-quantidade_litros = metros_quadrados/6
+quantidade_litros = metros_quadrados / 6
 # o método math.ceil retornará um valor inteiro arredondado para o próximo número
 quantidade_latas = math.ceil(quantidade_litros / litros_lata)
 quantidade_galoes = math.ceil(quantidade_litros / litros_galao)
-quantidade_mix = math.ceil()
-# TODO calcular a quantidade e valor dos mixes
+quantidade_mix_latas = int(quantidade_latas // 18)  # TODO rever esses cálculos
+quantidade_mix_galoes = math.ceil((quantidade_litros % 18) / 3.6)
+
 # calculando o valor dos casos (só latas, galões ou os dois juntos)
-valor_apenas_latas = preco_lata_18L*quantidade_latas
-valor_apenas_galoes = preco_galao_3_6L*quantidade_galoes
-valor_mix =
+valor_apenas_latas = preco_lata_18L * quantidade_latas
+valor_apenas_galoes = preco_galao_3_6L * quantidade_galoes
+valor_mix = (quantidade_mix_latas * 80) + (quantidade_mix_galoes * 18)
+
+print("Para aproveitar nossas promoções, é possível fazer suas compras nas seguintes modalidades:\n"
+      f"\n* Comprando apenas latas de 18L\n"
+      f"Quantidade de latas necessárias: {quantidade_latas}\n"
+      f"Total = R${valor_apenas_latas}\n"
+      f"\n* Comprando apenas galões de 3,6L\n"
+      f"Quantidade de galões necessários: {quantidade_galoes}\n"
+      f"Total = R${valor_apenas_galoes}\n"
+      f"\n* Comprando latas e galões\n"
+      f"Quantidade de latas necessárias: {quantidade_mix_latas}\n"
+      f"Quantidade de galões necessários: {quantidade_mix_galoes}\n"
+      f"Total = R${valor_mix}")
