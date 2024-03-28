f1dca4f28b1f2c657a8c907f165d2ccfa4f32bf4
Gabriel Dias <gabrielmdais557@gmail.com>
2024-03-28 15:21:23 -0300

    ac1_1 feita

--- CDL/AP1 - Linguagem de Programação II/Atividade Complementar 1/questão_1.py
+++ CDL/AP1 - Linguagem de Programação II/Atividade Complementar 1/questão_1.py
@@ -3,27 +3,31 @@
 # E MOSTRAR O RESULTADO. ENQUANTO O USUÁRIO NÃO DIGITAR A OPÇÃO SAIR O PROGRAMA CONTINUA MOSTRANDO O MENU AO FINAL DE
 # CADA OPERAÇÃO.
 
-print("MENU".center(33, "="), "\n")
+print("MENU".center(33, "="), "\nEscolha uma opção válida")
+menu = 1
+while menu != 0:
 
-
-print("Escolha uma opção válida")
-menu = int(input("[1] ADIÇÃO\n[2] SUBTRAÇÃO\n[3] MULTIPLICAÇÃO\n[4] DIVISÃO\n[0] SAIR\n"))
-if menu == 1:
-    numero_1 = int(input("Digite dois números para somar"))
-    numero_2 = int(input())
-    print(f"{numero_1} + {numero_2} = {numero_1 + numero_2}")
-elif menu == 2:
-    numero_1 = int(input("Digite dois números para subtrair"))
-    numero_2 = int(input())
-    print(f"{numero_1} - {numero_2} = {numero_1 - numero_2}")
-elif menu == 3:
-    numero_1 = int(input("Digite dois números para multiplicar"))
-    numero_2 = int(input())
-    print(f"{numero_1} x {numero_2} = {numero_1 * numero_2}")
-elif menu == 4:
-    numero_1 = int(input("Digite dois números para dividir"))
-    numero_2 = int(input())
-    print(f"{numero_1} : {numero_2} = {numero_1 / numero_2}")
-else:
-    print("Saindo...")
-
+    menu = int(input("[1] ADIÇÃO\n[2] SUBTRAÇÃO\n[3] MULTIPLICAÇÃO\n[4] DIVISÃO\n[0] SAIR\n"))
+    if menu == 1:
+        numero_1 = int(input("Digite dois números para somar\n"))
+        numero_2 = int(input())
+        print(f"{numero_1} + {numero_2} = {numero_1 + numero_2}")
+        continue
+    elif menu == 2:
+        numero_1 = int(input("Digite dois números para subtrair\n"))
+        numero_2 = int(input())
+        print(f"{numero_1} - {numero_2} = {numero_1 - numero_2}")
+        continue
+    elif menu == 3:
+        numero_1 = float(input("Digite dois números para multiplicar\n"))
+        numero_2 = float(input())
+        print(f"{numero_1} x {numero_2} = {numero_1 * numero_2}")
+        continue
+    elif menu == 4:
+        numero_1 = float(input("Digite dois números para dividir\n"))
+        numero_2 = float(input())
+        print(f"{numero_1} : {numero_2} = {numero_1 / numero_2:.1f}")
+        continue
+    elif menu == 0:
+        print("Saindo...")
+        break
