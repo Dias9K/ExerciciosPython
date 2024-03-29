91e60b5ca2a284f17999dd666819fc8cf15928a9
Gabriel Dias <gabrielmdais557@gmail.com>
2024-03-28 21:31:47 -0300

    ac1 completa

--- CDL/AP1 - Linguagem de Programação II/Atividade Complementar 1/questão_02.py
+++ CDL/AP1 - Linguagem de Programação II/Atividade Complementar 1/questão_02.py
@@ -6,42 +6,18 @@
 numero = int(input("Digite um número inteiro para verificar se ele é primo ou não: "))
 
 if numero < 0:
-    print(f"Digite apenas números inteiros e positivos.")
-elif numero <= 1:
+    print(f"Digite apenas números inteiros, positivos e maiores do que zero.")
+elif numero == 1:
     print(f"O número {numero} não é primo.")
-
+elif numero == 2:
+    print("O número 2 é o único par que é primo")
 else:
-    if numero == 2:
-        print(f"O {numero} é o único número par primo")
-    impar = 3
-    while impar < numero:
-        if numero % impar == 0:
+    primo = 3
+    while primo < numero:
+        if numero % primo == 0:
             break
-        impar += 2
-    if impar == numero:
+        primo += 2
+    if primo == numero:
         print(f"O {numero} é primo")
     else:
-        print(f"O {numero} não é primo, pois é divisível por {impar}")
-'''
-def eh_primo(n):  # Define uma função chamada 'eh_primo' que recebe um argumento 'n'.
-    if n < 2:  # Se 'n' for menor que 2,
-        return False  # retorna 'False'. Isso porque números menores que 2 não são primos.
-    if n == 2:  # Se 'n' for igual a 2,
-        return True  # retorna 'True'. Isso porque 2 é um número primo.
-    if n % 2 == 0:  # Se 'n' for divisível por 2 (ou seja, se 'n' for par),
-        return False  # retorna 'False'. Isso porque nenhum número par maior que 2 pode ser primo.
-    i = 3  # Define uma variável 'i' e atribui o valor 3 a ela.
-    while i * i <= n:  # Enquanto o quadrado de 'i' for menor ou igual a 'n',
-        if n % i == 0:  # se 'n' for divisível por 'i',
-            return False  # retorna 'False'. Isso porque 'n' tem um divisor além de 1 e ele mesmo, então 'n' não é primo.
-        i += 2  # Incrementa 'i' em 2. Isso efetivamente verifica apenas os números ímpares como possíveis divisores.
-    return True  # Se nenhum divisor foi encontrado, retorna 'True'. Isso significa que 'n' é primo.
-
-
-numero = int(input(
-    "Digite um número: "))  # Solicita ao usuário que digite um número e converte a entrada do usuário para um inteiro.
-if eh_primo(numero):  # Se o número for primo (ou seja, se a função 'eh_primo' retornar 'True'),
-    print(f"{numero} é um número primo.")  # imprime que o número é primo.
-else:  # Se o número não for primo (ou seja, se a função 'eh_primo' retornar 'False'),
-    print(f"{numero} não é um número primo.")  # imprime que o número não é primo.
-'''
+        print(f"O {numero} não é primo, pois é divisível por {primo}")
