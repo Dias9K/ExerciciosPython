b4667c2726231f38afd1ec7cf062f8f4cf737a46
Gabriel Dias <gabrielmdais557@gmail.com>
2024-04-02 22:41:52 -0300

    q3_489

--- CDL/Estrutura de Dados e Algoritmos/q3_estrutura_de_dados/vetores/q3_8_estrutura_de_dados.py
+++ CDL/Estrutura de Dados e Algoritmos/q3_estrutura_de_dados/vetores/q3_8_estrutura_de_dados.py
@@ -1,1 +1,18 @@
+# TODO fazer essa chibata rodar bem
 # Implemente um algoritmo para encontrar o segundo maior valor em um vetor.
+vetor = []
+
+for i in range(5):
+    numero = int(input("Digite um número: "))
+    vetor.append(numero)
+
+maior = float('-inf')
+segundo_maior = float('-inf')
+for valor in vetor:
+    if valor > maior:
+        segundo_maior = maior
+        maior = valor
+    elif valor > segundo_maior and valor != maior:
+        segundo_maior = valor
+
+print(f"Números inseridos no vetor {vetor}\nE o seu segundo maior elemento do vetor é = {segundo_maior}")
