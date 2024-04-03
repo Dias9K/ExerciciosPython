a954a477b4df2fd6b6340033d6a393994389af0b
Gabriel Dias <gabrielmdais557@gmail.com>
2024-04-03 10:16:43 -0300

    add pescador.py

--- CDL/Estrutura de Dados e Algoritmos/q3_estrutura_de_dados/matrizes/q3_1_estrutura_de_dados.py
+++ CDL/Estrutura de Dados e Algoritmos/q3_estrutura_de_dados/matrizes/q3_1_estrutura_de_dados.py
@@ -1,1 +1,7 @@
 # Implemente um algoritmo para encontrar o maior elemento em uma matriz.
+
+turma = [[5.0, 1.5, 2.0, 10.0, 7.0],
+         [5.5, 3.0, 9.0, 4.0, 6.0],
+         [1.5, 0.0, 5.0, 8.0, 7.5]]
+print(turma)
+# TODO terminar
