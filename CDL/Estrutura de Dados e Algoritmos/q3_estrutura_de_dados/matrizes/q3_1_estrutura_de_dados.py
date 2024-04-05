a0ca29eb4e2cac941154a2f755b6b0e90f860ecd
Gabriel Dias <gabrielmdais557@gmail.com>
2024-04-05 09:57:57 -0300

    matrizes_1

--- CDL/Estrutura de Dados e Algoritmos/q3_estrutura_de_dados/matrizes/q3_1_estrutura_de_dados.py
+++ CDL/Estrutura de Dados e Algoritmos/q3_estrutura_de_dados/matrizes/q3_1_estrutura_de_dados.py
@@ -3,5 +3,9 @@
 turma = [[5.0, 1.5, 2.0, 10.0, 7.0],
          [5.5, 3.0, 9.0, 4.0, 6.0],
          [1.5, 0.0, 5.0, 8.0, 7.5]]
-print(turma)
-# TODO terminar
+maior_elemento = 0
+for i in range(3):
+    for j in range(5):
+        if turma[i][j]:
+            maior_elemento = turma[i][j]
+print(f"O maior elemento é: {maior_elemento}")
