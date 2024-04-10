baf0729f98d76f2cbc27aa7eb4d729122e5c85f2
Gabriel Dias <gabrielmdais557@gmail.com>
2024-04-10 10:51:20 -0300

    q4_3 revisão

--- CDL/Estrutura de Dados e Algoritmos/revisão_ap1/vetores/q4_3_estrutura_de_dados.py
+++ CDL/Estrutura de Dados e Algoritmos/revisão_ap1/vetores/q4_3_estrutura_de_dados.py
@@ -1,2 +1,20 @@
 # Escreva um programa que encontre o maior e o menor valor em um vetor.
+
 import random
+
+vetor = []
+
+for i in range(10):
+    vetor.append(random.randint(1, 10))
+print(vetor)
+
+maior = vetor[0]
+menor = vetor[0]
+for numero in vetor:
+    if numero > maior:
+        maior = numero
+    if numero < menor:
+        menor = numero
+
+print(f"Maior número : {maior}")
+print(f"Menor número : {menor}")
