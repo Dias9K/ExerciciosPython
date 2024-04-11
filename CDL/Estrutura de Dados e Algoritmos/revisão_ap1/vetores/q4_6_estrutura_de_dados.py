9cd6f9602494a84a6ccfd8fd51e8e1edcb0e463d
Gabriel Dias <gabrielmdais557@gmail.com>
2024-04-11 10:58:05 -0300

    q4_6 revisão

--- CDL/Estrutura de Dados e Algoritmos/revisão_ap1/vetores/q4_6_estrutura_de_dados.py
+++ CDL/Estrutura de Dados e Algoritmos/revisão_ap1/vetores/q4_6_estrutura_de_dados.py
@@ -1,1 +1,19 @@
 # Implemente um algoritmo para encontrar o segundo maior valor em um vetor.
+import random
+
+vetor = []
+for i in range(10):
+    vetor.append(random.randint(1, 10))
+
+print(vetor)
+maior = 0
+segundo_maior = 0
+
+for numero in vetor:
+    if numero > maior:
+        segundo_maior = maior
+        maior = numero
+    elif numero > segundo_maior and numero != maior:
+        segundo_maior = numero
+
+print(f"O maior elemento do vetor é: {maior}\nE o segundo maior elemento é: {segundo_maior}")
