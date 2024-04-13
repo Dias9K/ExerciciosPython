04d686526c570217c27d2d3ef3dab77eb68d8389
Gabriel Dias <gabrielmdais557@gmail.com>
2024-04-13 00:33:39 -0300

    13. altura refatoração

--- ExercíciosPythonBrasil/estrutura_sequencial/13. altura_homem_mulher.py
+++ ExercíciosPythonBrasil/estrutura_sequencial/13. altura_homem_mulher.py
@@ -1,16 +1,21 @@
 """Tendo como dado de entrada a altura (h) de uma pessoa, construa um algoritmo que calcule seu peso ideal,
 utilizando as seguintes fórmulas: Para homens: (72.7*h) - 58 Para mulheres: (62.1*h) - 44.7"""
 
-sexo = input("Digite o seu sexo\n[M] - Masculino\n[F] - Feminino\n")
-# TODO verificar se a entrada de sexo é válida ou não
-altura = float(input("Digite a sua altura: "))
+while True:
+    sexo = input("Digite o seu sexo\n[M] - Masculino\n[F] - Feminino\n")
+    if sexo[0].lower() != "m" and sexo[0].lower() != "f":
+        print("Sexo inválido! Digite apenas as entradas sugeridas.")
+# TODO fazer dar loop apenas para retornar apenas para corrigir a opção inválida, não o código inteiro
+    elif sexo[0].lower() == "m" or sexo[0].lower() == "f":
+        while True:
+            try:
+                altura = float(input("Digite a sua altura: "))
+                if sexo[0].lower() == "m":
+                    print(f"O peso ideal para o sexo masculino é: {(72.7 * altura) - 58:.2f}")
+                    break
+                elif sexo[0].lower() == "f":
+                    print(f"O peso ideal para o sexo feminino é: {(62.1 * altura) - 44.7:.3f}")
+                    break
+            except ValueError:
+                print("Valor inválido! Digite apenas inteiros ou flutuantes.")
 
-while sexo[0].lower() == "m" or sexo[0].lower() == "f":
-    if sexo == "m":
-        print(f"O peso ideal para o sexo masculino é: {(72.7 * altura) - 58:.3f}")
-        break
-    elif sexo == "f":
-        print(f"O peso ideal para o sexo feminino é: {(62.1 * altura) - 44.7:.3f}")
-        break
-    else:
-        print("Opção inválida!")
