85913d7d6871be7250d7e6ddf87fb6e6bc9c5f1b
Gabriel Dias <gabrielmdais557@gmail.com>
2024-04-17 10:20:55 -0300

    ordenacao.py

--- CDL/Estrutura de Dados e Algoritmos/ordenacao.py
+++ CDL/Estrutura de Dados e Algoritmos/ordenacao.py
@@ -1,3 +1,10 @@
 # ordenar o vetor sem utilizar o método .sort()
 vetor = [2, 3, 1, 5, 4]
 
+for i in range(len(vetor)):
+    for j in range(i+1, len(vetor)):
+        if vetor[i] > vetor[j]:
+            menor = vetor[i]
+            vetor[i] = vetor[j]
+            vetor[j] = menor
+print(vetor)
