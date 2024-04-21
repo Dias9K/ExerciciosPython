5f176fa8211b407eec65ef1edfea3cb8d64fa1cb
Gabriel Dias <gabrielmdais557@gmail.com>
2024-04-21 19:27:32 -0300

    fatoracao.py2

--- fatoracao.py
+++ fatoracao.py
@@ -1,6 +1,11 @@
 divisores = []
-
+dividendo = 2
 for i in range(3):
     numero = input("Digite o números para fazer a fatoração: ")
     divisores.append(numero)
-print(divisores)
+
+for numero in divisores:
+    if numero % dividendo == 0:
+        print(f"O número é divisível por {dividendo}")
+    else:
+        dividendo += 1
