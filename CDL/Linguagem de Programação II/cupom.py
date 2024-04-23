e56354b623e4e781c6f66b1533c17ce462daa821
Gabriel Dias <gabrielmdais557@gmail.com>
2024-04-23 11:12:03 -0300

    cupom.py6

--- CDL/Linguagem de Programação II/cupom.py
+++ CDL/Linguagem de Programação II/cupom.py
@@ -3,11 +3,12 @@ menu = 0
 item = ""
 quantidade = 0
 valor_unitario = 0
-while menu != 4:
+while menu != 5:
     print("MENU".center(20, ":"), "\n[1] Adicionar novo item ao carrinho\n"
                                   "[2] Checar quantidade de um determinado item\n"
                                   "[3] Checar valor total das unidades de um determinado item\n"
-                                  "[4] Sair")
+                                  "[4] Conferir os itens comprados, seus valores e quantidades\n"
+                                  "[5] Sair")
     try:
         menu = int(input("Digite uma opção: "))
     except ValueError:
@@ -20,16 +21,18 @@ while menu != 4:
             carrinho.append([item, quantidade, valor_unitario])
         else:
             print("ERRO! É obrigatório digitar um item!")
+
     elif menu == 2:
         item_checado = input("Digite o item a ser checado: ")
         if len(item) == 0 or item_checado != item:
             print("O carrinho de compras está vazio ou o item digitado não foi encontrado")
         elif item_checado == item:
-            print(f"{item} == {quantidade}")
+            print(f"Você comprou {quantidade} de {item}")
+
     elif menu == 3:
         item_checado = input("Digite o item para ver a o seu valor total: ")
         if item_checado == item:
-            print(f"{item} == {valor_unitario * quantidade}")
+            print(f"O total de {item} é {quantidade * valor_unitario}")
         elif item_checado != item:
             print(f"Item não encontrado no carrinho")
     else:
