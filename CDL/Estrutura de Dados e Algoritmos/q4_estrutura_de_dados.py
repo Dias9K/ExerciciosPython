da2c513bc32d9de96e008ea99bc9647b6541c898
Gabriel Dias <gabrielmdais557@gmail.com>
2024-04-23 21:22:56 -0300

    valendo_livro (corrigido 100%)

--- CDL/Estrutura de Dados e Algoritmos/q4_estrutura_de_dados.py
+++ CDL/Estrutura de Dados e Algoritmos/q4_estrutura_de_dados.py
@@ -1,13 +1,30 @@
 import random
 
-vetor = []
+vetor_desordenado = []
 for i in range(0, 20):
-    vetor.append(random.randint(1, 20))
+    vetor_desordenado.append(random.randint(1, 20))
 
-for i in range(len(vetor)):
-    for j in range(i+1, len(vetor)):
-        if vetor[i] > vetor[j]:
-            menor = vetor[i]
-            vetor[i] = vetor[j]
-            vetor[j] = menor
-print(vetor)
+print("Desordenado")
+print(vetor_desordenado)
+
+# Ordenação crescente
+for i in range(len(vetor_desordenado)):
+    for j in range(i + 1, len(vetor_desordenado)):
+        if vetor_desordenado[i] > vetor_desordenado[j]:
+            vetor_desordenado[i], vetor_desordenado[j] = vetor_desordenado[j], vetor_desordenado[i]
+
+# Primeiros 10 elementos em ordem crescente
+vetor_1 = vetor_desordenado[:10]
+print("Ordem crescente")
+print(vetor_1)
+
+# Ordenação decrescente
+for i in range(len(vetor_desordenado)):
+    for j in range(i + 1, len(vetor_desordenado)):
+        if vetor_desordenado[i] < vetor_desordenado[j]:
+            vetor_desordenado[i], vetor_desordenado[j] = vetor_desordenado[j], vetor_desordenado[i]
+
+# Primeiros 10 elementos em ordem decrescente
+vetor_2 = vetor_desordenado[:10]
+print("Ordem decrescente")
+print(vetor_2)
