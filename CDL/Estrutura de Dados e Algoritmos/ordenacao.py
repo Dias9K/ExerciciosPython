20f655ac953c81077e3f32d9feef8ca82888064e
Gabriel Dias <gabrielmdais557@gmail.com>
2024-04-26 01:39:58 -0300

    comentários bubble sort

--- CDL/Estrutura de Dados e Algoritmos/ordenacao.py
+++ CDL/Estrutura de Dados e Algoritmos/ordenacao.py
@@ -1,7 +1,6 @@
 # ordenar o vetor sem utilizar o método .sort()
 vetor = [2, 3, 1, 5, 4]
 
-# parece um bubble sort
 for i in range(len(vetor)):
     for j in range(i+1, len(vetor)):
         if vetor[i] > vetor[j]:
--- CDL/Estrutura de Dados e Algoritmos/q4_estrutura_de_dados.py
+++ CDL/Estrutura de Dados e Algoritmos/q4_estrutura_de_dados.py
@@ -12,9 +12,10 @@ print(vetor_desordenado)
 for i in range(len(vetor_desordenado)):
     for j in range(i + 1, len(vetor_desordenado)):
         if vetor_desordenado[i] > vetor_desordenado[j]:
+            # "Atribuição dupla" pois a posição de dois valores do vetor desordenado estão sendo trocadas ao mesmo tempo
             vetor_desordenado[i], vetor_desordenado[j] = vetor_desordenado[j], vetor_desordenado[i]
 
-# Primeiros 10 elementos em ordem crescente
+# Primeiros 10 elementos em ordem crescente com fatiamento de listas
 vetor_1 = vetor_desordenado[:10]
 print("Ordem crescente")
 print(vetor_1)
@@ -27,5 +28,6 @@ for i in range(len(vetor_desordenado)):
 
 # Primeiros 10 elementos em ordem decrescente
 vetor_2 = vetor_desordenado[:10]
+# O fatiamento não é capaz de remover os elementos restantes do vetor que foi fatiado
 print("Ordem decrescente")
 print(vetor_2)
