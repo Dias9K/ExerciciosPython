f9c9edc637f4737e9c23796c0c4d5da093818a6b
Gabriel Dias <gabrielmdais557@gmail.com>
2024-04-29 10:21:58 -0300

    rifa.py2

--- rifa.py
+++ rifa.py
@@ -10,3 +10,7 @@ print(f"Há {len(numeros_disponiveis)} números disponíveis na rifa")
 for i in range(6):
     numeros_escolhidos.append(numeros_disponiveis[random.randint(0, len(numeros_disponiveis) - 1)])
     print(f"{i+1} número escolhido {numeros_escolhidos[i]}")
+
+# alternativa
+# numeros_escolhidos.append(random.sample(numeros_disponiveis, 6))
+# print(f"Os 6 números escolhidos foram: {numeros_escolhidos}")
