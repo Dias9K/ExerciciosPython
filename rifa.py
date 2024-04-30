6c42b8bc2b8d0b278155f6bb957b6e8d5c80b2d0
Gabriel Dias <gabrielmdais557@gmail.com>
2024-04-30 03:09:00 -0300

    rifa.py3

--- rifa.py
+++ rifa.py
@@ -1,8 +1,7 @@
 import random
 
-numeros_disponiveis = [2, 3, 4, 6, 8, 10, 11, 15, 18, 20, 22, 25, 28, 29, 30, 31, 34, 35, 36, 37, 38, 40, 41, 42, 43,
-                       44, 45, 46, 47, 48, 49, 50, 51, 52,
-                       53, 54, 55, 56, 57, 58, 59, 60]
+numeros_disponiveis = []
+quantidade = int(input("Quantos números estão disponíveis na rifa"))
 
 numeros_escolhidos = []
 print(f"Há {len(numeros_disponiveis)} números disponíveis na rifa")
