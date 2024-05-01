79534e13b0947722d16d3e46079dd40e116c8824
Gabriel Dias <gabrielmdais557@gmail.com>
2024-05-01 00:23:42 -0300

    bubble_sort.py1

--- CDL/Estrutura de Dados e Algoritmos/bubble_sort.py
+++ CDL/Estrutura de Dados e Algoritmos/bubble_sort.py
@@ -1,18 +1,16 @@
-
-
 def bubble_sort(arr):
     # percorrendo todos os elemetos do array
-    for i in range(len(arr)):
+    total_vetor = len(arr)
+    for i in range(total_vetor):
         # últimos i elementos já estão no lugar certo
-        for j in range(0, len(arr) - i - 1):
+        for j in range(i, total_vetor - 1):
             # troca se o elemento encontrado for maior que o próximo elemento
             if arr[j] > arr[j + i]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
 
 # exemplos
-arr = [66, 34, 25, 12, 22, 11, 90]
-bubble_sort(arr)
-print("Array ordenado:")
-for i in range(len(arr)):
-    print("%d" % arr[i], end=" ")
+vetor = [66, 34, 25, 12, 22, 11, 90]
+bubble_sort(vetor)
+print(vetor)
+# https://www.kabum.com.br/produto/441114/placa-de-video-pcyes-nvidia-geforce-g-210-1-gb-ddr3-pvg2101gbr364lp
