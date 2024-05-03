e933c0d594f15d97685fb69b281f073dac1e3435
Gabriel Dias <gabrielmdais557@gmail.com>
2024-05-03 18:47:21 -0300

    rifa.py6

--- rifa.py
+++ rifa.py
@@ -1,14 +1,14 @@
 # import random
 
 numeros_disponiveis = []
-quantidade = int(input("Quantos números estão disponíveis na rifa?\n"))
-disponiveis = []
-for i in range(quantidade):
-    disponiveis = input("Digite um número disponível na rifa")
+quantidade_disponiveis = int(input("Quantos números estão disponíveis na rifa?\n"))
+for i in range(quantidade_disponiveis):
+    disponiveis = int(input("Digite o número disponível na rifa"))
     numeros_disponiveis.append(disponiveis)
-# TODO colocar quantos números você quer sortear
+quantidade_pontos = int(input("Quantos pontos você quer colocar na rifa?"))
+
 numeros_escolhidos = []
-print(f"Há {len(numeros_disponiveis)} números disponíveis na rifa")
+# TODO gerar números aleatórios
 
 # for i in range(6):
 #     numeros_escolhidos.append(numeros_disponiveis[random.randint(0, len(numeros_disponiveis) - 1)])
