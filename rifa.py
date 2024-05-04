b18ec20bcc5cd3d804796282a4d728eca0e3c34c
Gabriel Dias <gabrielmdais557@gmail.com>
2024-05-04 03:08:52 -0300

    rifa.py8 (quase completa)

--- rifa.py
+++ rifa.py
@@ -2,20 +2,21 @@ import random
 
 numeros_disponiveis = []
 quantidade_disponiveis = int(input("Quantos números estão disponíveis na rifa?\n"))
+print("Digite quais números estão disponíveis na rifa")
+
 for i in range(quantidade_disponiveis):
-    print("Digite quais números estão disponíveis na rifa")
     numeros = int(input())
     numeros_disponiveis.append(numeros)
-quantidade_pontos = int(input("Quantos pontos você quer colocar na rifa?"))
 
+quantidade_pontos = int(input("Quantos pontos você quer colocar na rifa?\n"))
 numeros_escolhidos = []
+
 for i in range(len(numeros_disponiveis)):
-    numeros_escolhidos.append(random)
-# TODO gerar números aleatórios com base nos números disponíveis
-# for i in range(6):
+    numeros_escolhidos.append(random.sample(numeros_disponiveis, quantidade_pontos))
+
+print(f"Os {quantidade_pontos} números escolhidos foram: {numeros_escolhidos[
+    random.randint(0, len(numeros_escolhidos)-1)]}")
+
+# for i in range(6): sei lá que foi que eu fiz aqui
 #     numeros_escolhidos.append(numeros_disponiveis[random.randint(0, len(numeros_disponiveis) - 1)])
 #     print(f"{i+1} número escolhido {numeros_escolhidos[i]}")
-
-# alternativa
-# numeros_escolhidos.append(random.sample(numeros_disponiveis, 6))
-# print(f"Os 6 números escolhidos foram: {numeros_escolhidos}")
