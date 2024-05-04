3dcaa9514b096e97efad7c11f398bef44d347465
Gabriel Dias <gabrielmdais557@gmail.com>
2024-05-04 12:27:32 -0300

    rifa.py9

--- rifa.py
+++ rifa.py
@@ -2,21 +2,21 @@ import random
 
 numeros_disponiveis = []
 quantidade_disponiveis = int(input("Quantos números estão disponíveis na rifa?\n"))
-print("Digite quais números estão disponíveis na rifa")
 
+print("Digite quais números estão disponíveis na rifa")
 for i in range(quantidade_disponiveis):
     numeros = int(input())
-    numeros_disponiveis.append(numeros)
-
+    if numeros in numeros_disponiveis:
+        print("O número digitado já existe")
+    else:
+        numeros_disponiveis.append(numeros)
+        # quase perfeito mais ainda não está
+print(f"O números disponíveis na rifa são {numeros_disponiveis}")
 quantidade_pontos = int(input("Quantos pontos você quer colocar na rifa?\n"))
 numeros_escolhidos = []
 
-# TODO implementar condicionais pra não quebrar o código
 for i in range(len(numeros_disponiveis)):
     numeros_escolhidos.append(random.sample(numeros_disponiveis, quantidade_pontos))
+print(numeros_disponiveis)
 print(f"Os {quantidade_pontos} números escolhidos foram: {numeros_escolhidos[
     random.randint(0, len(numeros_escolhidos)-1)]}")
-
-# for i in range(6): sei lá que foi que eu fiz aqui
-#     numeros_escolhidos.append(numeros_disponiveis[random.randint(0, len(numeros_disponiveis) - 1)])
-#     print(f"{i+1} número escolhido {numeros_escolhidos[i]}")
