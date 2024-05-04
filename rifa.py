dc650705f9136995614b773459b10880d703fcf3
Gabriel Dias <gabrielmdais557@gmail.com>
2024-05-04 03:10:24 -0300

    rifa.py8 (# TODO)

--- rifa.py
+++ rifa.py
@@ -11,9 +11,9 @@ for i in range(quantidade_disponiveis):
 quantidade_pontos = int(input("Quantos pontos você quer colocar na rifa?\n"))
 numeros_escolhidos = []
 
+# TODO implementar condicionais pra não quebrar o código
 for i in range(len(numeros_disponiveis)):
     numeros_escolhidos.append(random.sample(numeros_disponiveis, quantidade_pontos))
-
 print(f"Os {quantidade_pontos} números escolhidos foram: {numeros_escolhidos[
     random.randint(0, len(numeros_escolhidos)-1)]}")
 
