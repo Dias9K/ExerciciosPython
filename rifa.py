4674e0ab72009eb81296e8d97f9872caa9d4377b
Gabriel Dias <gabrielmdais557@gmail.com>
2024-05-06 09:45:13 -0300

    rifa.py10 (descrições)

--- rifa.py
+++ rifa.py
@@ -6,26 +6,32 @@ numeros_disponiveis = []
 quantidade_disponiveis = int(input("Quantos números estão disponíveis na rifa?\n"))
 
 print("Digite quais números estão disponíveis na rifa")
+# insere os números e verifica se há números repetidos na lista
 for i in range(quantidade_disponiveis):
     numeros = int(input())
+    # se o número digitado não estiver na lista, ele será adicionado
     if numeros not in numeros_disponiveis:
         numeros_disponiveis.append(numeros)
     else:
         while True:
+            # se estiver, entra nesse laço infinito e só sai quando o número digitado não houver na lista
             numeros = int(input("O número digitado já existe. Digite um número diferente para que ele possa ser "
                                 "adicionado\n"))
             if numeros not in numeros_disponiveis:
                 numeros_disponiveis.append(numeros)
                 break
-        # quase perfeito mais ainda não está
+
 print(f"O números disponíveis na rifa são {numeros_disponiveis}")
 
 numeros_escolhidos = []
 while True:
+    # verificação para checar se a quantidade de números desejados é maior do que a quantidade de números disponíveis
     quantidade_pontos = int(input("Quantos pontos você quer colocar na rifa?\n"))
     if quantidade_pontos > len(numeros_disponiveis):
         print("Quantidade inválida, pois é maior do que a quantidade de números disponíveis")
     else:
+        # o sample pega da lista 'numeros_disponiveis' a mesma quantidade de 'quantidade_pontos' e os armazena na
+        # lista 'numeros_escolhidos'
         numeros_escolhidos.append(random.sample(numeros_disponiveis, quantidade_pontos))
         break
 
