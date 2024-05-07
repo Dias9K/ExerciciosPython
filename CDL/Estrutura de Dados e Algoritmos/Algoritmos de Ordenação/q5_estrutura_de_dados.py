bd6ca080782607da630fe7d4457170969f8d6211
Gabriel Dias <gabrielmdais557@gmail.com>
2024-05-07 14:52:41 -0300

    q5_estrutura_de_dados.py (concluída)

--- CDL/Estrutura de Dados e Algoritmos/Algoritmos de Ordenação/q5_estrutura_de_dados.py
+++ CDL/Estrutura de Dados e Algoritmos/Algoritmos de Ordenação/q5_estrutura_de_dados.py
@@ -3,25 +3,44 @@ import time
 
 inicio_time = time.time()
 
-# atribuição dos 1000 elementos no vetor
-vetor = []
-for i in range(0, 1000):
-    vetor.append(random.randint(1, 1000))
+# atribuição dos 1000 elementos no vetor usado pelo bubble sort
+vetor_bubble = []
+for i in range(0, 10000000):
+    vetor_bubble.append(random.randint(1, 1000))
+
+# atribuição dos 1000 elementos no vetor usado pelo selection sort
+vetor_selection = []
+for i in range(0, 10000000):
+    vetor_selection.append(random.randint(1, 1000))
+
+
+def bubble_sort(arr):
+    # função com bubble
+    for i in range(len(arr)):
+        for j in range(i + 1, len(arr)):
+            if arr[i] > arr[j]:
+                arr[i], arr[j] = arr[j], arr[i]
+    return arr
 
 
 def selection_sort(arr):
     # função com selection
-    total = len(arr)
-    for i in range(total):
+    for i in range(len(arr)):
         idx_min = i
-        for j in range(i + 1, total):
+        for j in range(i + 1, len(arr)):
             if arr[j] < arr[idx_min]:
                 arr[j], arr[idx_min] = arr[idx_min], arr[j]
     return arr
 
 
-selection_sort(vetor)
-print(selection_sort(vetor))
-selection_time = time.time()
+# chamada da função bubble sort
+bubble_sort(vetor_bubble)
+print(bubble_sort(vetor_bubble))
+bubble_time = time.time()
+print("Tempo de execução do algoritmo bubble_sort = {:.2f}s".format(bubble_time - inicio_time))
 
-print(selection_time - inicio_time)
+# chamada da função selection sort
+selection_sort(vetor_selection)
+print(selection_sort(vetor_selection))
+selection_time = time.time()
+print("Tempo de execução do algoritmo selection_sort = {:.2f}s".format(selection_time - inicio_time))
