4e43064318f449904f4987c72b1b2fd277dc826d
Gabriel Dias <gabrielmdais557@gmail.com>
2024-05-07 14:03:15 -0300

    bubble_sort.py ?

--- CDL/Estrutura de Dados e Algoritmos/Algoritmos de Ordenação/bubble_sort.py
+++ CDL/Estrutura de Dados e Algoritmos/Algoritmos de Ordenação/bubble_sort.py
@@ -3,9 +3,9 @@ def bubble_sort(arr):
     total_vetor = len(arr)
     for i in range(total_vetor):
         # últimos i elementos já estão no lugar certo
-        for j in range(i, total_vetor - 1):
+        for j in range(0, total_vetor-i-1):
             # troca se o elemento encontrado for maior que o próximo elemento
-            if arr[j] > arr[j + i]:
+            if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
 
@@ -13,4 +13,3 @@ def bubble_sort(arr):
 vetor = [66, 34, 25, 12, 22, 11, 90]
 bubble_sort(vetor)
 print(vetor)
-# https://www.kabum.com.br/produto/441114/placa-de-video-pcyes-nvidia-geforce-g-210-1-gb-ddr3-pvg2101gbr364lp
