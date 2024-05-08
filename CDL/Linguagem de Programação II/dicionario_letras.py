96e98a0c7164dc6647eb9e3ec33c80585f3c1bf5
Gabriel Dias <gabrielmdais557@gmail.com>
2024-05-08 20:05:20 -0300

    dicionario_letras.py2

--- CDL/Linguagem de Programação II/dicionario_letras.py
+++ CDL/Linguagem de Programação II/dicionario_letras.py
@@ -2,8 +2,10 @@ d = {}
 palavra = "paralelepipedo"
 for letra in palavra:
     if letra in d:
-        d[letra] = d[letra] +1
+        # se a letra já estiver no dicionário, é somado +1
+        d[letra] = d[letra] + 1
     else:
+        # se não houver atribui a letra e atribui 1
         d[letra] = 1
 
 print(d)
