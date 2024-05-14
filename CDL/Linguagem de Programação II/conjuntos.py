4234a7e6b6dbbf6a4d084c8a22aefb464d66a199
Gabriel Dias <gabrielmdais557@gmail.com>
2024-05-14 03:11:01 -0300

    conjuntos.py5

--- CDL/Linguagem de Programação II/conjuntos.py
+++ CDL/Linguagem de Programação II/conjuntos.py
@@ -5,7 +5,7 @@
 # d) uma lista com os valores não repetidos das duas listas;
 # e) a primeira lista sem os valores repetidos da segunda lista.
 
-lista_1 = [1, 2, 3, 4, 5]
+lista_1 = [1, 2, 3, 6, 4, 5]
 lista_2 = [5, 4, 7, 8, 10]
 
 lista_1 = set(lista_1)
@@ -14,4 +14,4 @@ print(f"valores comuns das duas listas\n{lista_1.intersection(lista_2)}")
 print(f"valores existentes apenas na primeira lista\n{lista_1.difference(lista_2)}")
 print(f"valores que existem apenas na segunda lista\n{lista_2.difference(lista_1)}")
 print(f"valores não repetidos das duas listas\n{lista_1.symmetric_difference(lista_2)}")
-# TODO lista sem os valores repetidos da segunda lista
+print(f"valores da primeira lista sem os repetidos da segunda lista\n{lista_2.symmetric_difference(lista_1)}")
