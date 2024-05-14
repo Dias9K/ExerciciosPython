a70305c55bbb24930d231a6b811c687fd0061cb4
Gabriel Dias <gabrielmdais557@gmail.com>
2024-05-14 20:24:45 -0300

    binaria_1 correção

--- CDL/Estrutura de Dados e Algoritmos/Algoritmos de Busca/buscaBinaria.py
+++ CDL/Estrutura de Dados e Algoritmos/Algoritmos de Busca/buscaBinaria.py
@@ -1,18 +1,18 @@
 def buscaBinaria(vetor_ordenado, chave):
     for i in range(len(vetor_ordenado)):
         inicio = 0
-        fim = vetor_ordenado[-1]
+        fim = len(vetor_ordenado) - 1
 
-        while len(vetor_ordenado) >= 1:
-            meio = (inicio + fim) / 2
+        while inicio <= fim:
+            meio = (inicio + fim) // 2
             if vetor_ordenado[meio] == chave:
                 return meio
             elif vetor_ordenado[meio] > chave:
                 fim = meio - 1
                 return meio
-            elif vetor_ordenado[meio] < chave:
+            else:
                 inicio = meio + 1
-                return meio
+        return -1
 
 
 print(buscaBinaria([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 5))
