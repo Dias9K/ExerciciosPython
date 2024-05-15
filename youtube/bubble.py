9744ac50cfcc7f768ee807aad902de0f4460181f
Gabriel Dias <gabrielmdais557@gmail.com>
2024-05-15 20:39:57 -0300

    bubble.py3

--- youtube/bubble.py
+++ youtube/bubble.py
@@ -1,5 +1,10 @@
 import random
 
+
+def bubble_sort(v):
+    fim = len(v)  # determina a quantidade total do vetor e atribui como o fim
+
+
 vetor = list(range(0, 10))
 random.shuffle(vetor)
 print(vetor)
