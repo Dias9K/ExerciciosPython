499754f71e4f7515da269686b198851b9d95c7fd
Gabriel Dias <gabrielmdais557@gmail.com>
2024-05-15 20:19:15 -0300

    funcoes cdl2

--- CDL/Linguagem de Programação II/funcoes.py
+++ CDL/Linguagem de Programação II/funcoes.py
@@ -1,4 +1,11 @@
 frase = "um tigre, dois tigres, tres tigres"
-print(f"Primeira ocorrência: {frase.find("tigre")}")
-print(f"Segunda ocorrência: {frase.find("tigres")}")
-print(f"Segunda ocorrência: {frase.find("tigres", 16)}")
+# print(f"Primeira ocorrência: {frase.find("tigre")}")
+# print(f"Segunda ocorrência: {frase.find("tigres")}")
+# print(f"Segunda ocorrência: {frase.find("tigres", 16)}")
+
+p = 0
+while p > -1:
+    p = frase.find("tigre", p)
+    if p >= 0:
+        print(p)
+        p += 1
