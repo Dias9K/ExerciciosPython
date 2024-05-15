cca14abca559d09d82e5349c0aa72ad2b01f6cc6
Gabriel Dias <gabrielmdais557@gmail.com>
2024-05-15 19:23:55 -0300

    funcoes cdl

---
+++ CDL/Linguagem de Programação II/funcoes.py
@@ -0,0 +1,4 @@
+frase = "um tigre, dois tigres, tres tigres"
+print(f"Primeira ocorrência: {frase.find("tigre")}")
+print(f"Segunda ocorrência: {frase.find("tigres")}")
+print(f"Segunda ocorrência: {frase.find("tigres", 16)}")
