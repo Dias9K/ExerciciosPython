ab1daf55dfeb7d3ccd735543e37c6c2c6cd46674
Gabriel Dias <gabrielmdais557@gmail.com>
2024-05-15 21:27:29 -0300

    atividade_string.py1

--- CDL/Linguagem de Programação II/atividade_string.py
+++ CDL/Linguagem de Programação II/atividade_string.py
@@ -3,3 +3,17 @@
 # T: 2x
 # A: 2x
 # C: 1x
+
+string = list(input("Digite uma palavra qualquer: "))  # a palavra de entrada do usuário se torna uma lista
+contador_caracteres = {}
+
+print(f"Elementos contidas na lista: {string}")
+
+for elemento in contador_caracteres:
+    if elemento in string:
+        contador_caracteres[elemento] += 1
+    else:
+        contador_caracteres[elemento] = 1
+for elemento, quantidade in contador_caracteres.items():
+    print(elemento, quantidade)
+# TODO terminar
