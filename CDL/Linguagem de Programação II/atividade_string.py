5ea02e500d7ebbabca8cbecfb56d8e29bead0477
Gabriel Dias <gabrielmdais557@gmail.com>
2024-05-15 22:34:47 -0300

    atividade_string.py2 finalizada

--- CDL/Linguagem de Programação II/atividade_string.py
+++ CDL/Linguagem de Programação II/atividade_string.py
@@ -4,16 +4,16 @@
 # A: 2x
 # C: 1x
 
-string = list(input("Digite uma palavra qualquer: "))  # a palavra de entrada do usuário se torna uma lista
-contador_caracteres = {}
+string = list(input("Digite qualquer palavra: "))  # as letras da palavra de entrada do usuário se tornam elementos
+# de uma lista
+contador_caracteres = {}  # dicionário vazio
 
 print(f"Elementos contidas na lista: {string}")
 
-for elemento in contador_caracteres:
-    if elemento in string:
-        contador_caracteres[elemento] += 1
+for elemento in string:  # itera sobre os elementos da lista
+    if elemento in contador_caracteres:  # condicionais para adicionar elementos ao contador
+        contador_caracteres[elemento] += 1  # caso o elemento já esteja no contador, é somado mais 1
     else:
-        contador_caracteres[elemento] = 1
+        contador_caracteres[elemento] = 1  # caso não esteja, o elemento é inicializado em 1
 for elemento, quantidade in contador_caracteres.items():
-    print(elemento, quantidade)
-# TODO terminar
+    print(f"O elemento \"{elemento}\" aparece {quantidade}x")
