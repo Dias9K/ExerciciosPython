31286f62492cb0de767c1647709dd2e73ba4aba9
Gabriel Dias <gabrielmdais557@gmail.com>
2024-05-19 01:21:06 -0300

    bubble.py3

--- youtube/bubble.py
+++ youtube/bubble.py
@@ -9,17 +9,19 @@ def bubble_sort(v):
     while fim > 0:  # enquanto a lista ainda tiver um tamanho, a lista vai sendo ordenada e o "fim" vai diminuindo
         i = 0
         # percorrendo o vetor de 0 até fim
-        while i < fim - 1:
+        while i < fim - 1:  # a verificação precisa ser feita até um valor a menos que o fim, para que não seja
+            # acessado um índice fora da lista
             if v[i] > v[i + 1]:
                 # fazendo swap da posição atual pela próxima
                 temp = v[i]
                 v[i] = v[i + 1]
                 v[i + 1] = temp
+                print(v)
             i += 1
         fim -= 1
 
 
 vetor = list(range(0, 10))
 random.shuffle(vetor)
+print(f"Desordenado\n{vetor}")
 bubble_sort(vetor)
-print(vetor)
