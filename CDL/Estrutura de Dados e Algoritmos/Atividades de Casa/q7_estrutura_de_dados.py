f161fbf0e9119cbe06569f041c753a92ea4ab876
Gabriel Dias <gabrielmdais557@gmail.com>
2024-05-21 18:08:48 -0300

    q7_estrutura_de_dados.py3

--- CDL/Estrutura de Dados e Algoritmos/Atividades de Casa/q7_estrutura_de_dados.py
+++ CDL/Estrutura de Dados e Algoritmos/Atividades de Casa/q7_estrutura_de_dados.py
@@ -1,6 +1,16 @@
 # TODO escolher um algoritmo de ordenação e usar um de busca em um vetor com mil elementos
-import random
+
+def binary_search(v, i, f, e):
+    if i <= f:
+        m = (i + f) // 2
+        if e > v[m]:
+            return binary_search(v, m + 1, f, e)
+        if e < v[m]:
+            return binary_search(v, i, m - 1, e)
+        else:
+            return m
+    return -1
+
 
 vetor = list(range(1, 1000))
-random.shuffle(vetor)
-print(vetor)
+print(binary_search(vetor, vetor[0], vetor[len(vetor) - 1], 100))
