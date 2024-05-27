4e8cc4e90040c84e5dfddb86401514a7880021d3
Gabriel Dias <gabrielmdais557@gmail.com>
2024-05-27 10:19:38 -0300

    selection.py3

--- youtube/selection.py
+++ youtube/selection.py
@@ -4,16 +4,17 @@ import random
 
 def selection_sort(v):
     i = 0
-    while i < len(v) - 1:  # o i vai percorrer até a penúltima posição do vetor, por isso é menor que é o tamanho -1
+    # indica inicialmente o menor elemento
+    while i < len(v) - 1:  # o i vai percorrer até a penúltima posição do vetor, por isso é menor que o tamanho -1
         menor = i  # a cada loop é atribuído o i como o índice de menor valor
         j = i + 1  # o j sempre estará a uma posição na frente do i
 
-        # verificando o menor elemento
+        # verificando se o i (menor) realmente é o menor elemento
         while j < len(v):
-            if v[j] < v[menor]:
+            if v[j] < v[menor]:  # caso o j seja menor do que o menor atual, menor receberá o valor de j
                 menor = j
             j += 1
-        if menor != i:
+        if menor != i:  # caso a variável menor seja diferente que i, a troca é feita
             temp = v[i]
             v[i] = v[menor]
             v[menor] = temp
@@ -23,4 +24,3 @@ def selection_sort(v):
 vetor = list(range(0, 10))
 random.shuffle(vetor)
 print(vetor)
-# TODO terminar isso
