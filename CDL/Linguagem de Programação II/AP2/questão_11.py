f9df473fe1045db4c4ec1b83218f159f3b4efd35
Gabriel Dias <gabrielmdais557@gmail.com>
2024-06-06 11:26:03 -0300

    não sei resolver essa e tô vendo de outros cantos

--- CDL/Linguagem de Programação II/AP2/questão_11.py
+++ CDL/Linguagem de Programação II/AP2/questão_11.py
@@ -2,3 +2,16 @@
 # Questão 11 – (0,75 pontos)
 # Escreva um programa que recebe o nome de um arquivo pela linha de comando e que
 # imprima todas as linhas deste arquivo.
+
+import sys
+
+if len(sys.argv) != 2:  # Lembre-se que o nome do programa é o primeiro da lista
+    print("\nUso: e09-01.py nome_do_arquivo\n\n")
+else:
+    nome = sys.argv[1]
+    arquivo = open(nome, "r")
+    for linha in arquivo.readlines():
+        # Como a linha termina com ENTER,
+        # retiramos o último caractere antes de imprimir
+        print(linha[:-1])
+    arquivo.close()
