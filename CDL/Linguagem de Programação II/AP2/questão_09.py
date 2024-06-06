f7af3aac8942f698358ca362feb251d703aa367d
Gabriel Dias <gabrielmdais557@gmail.com>
2024-06-05 23:04:31 -0300

    ap2questão_09.py

--- CDL/Linguagem de Programação II/AP2/questão_09.py
+++ CDL/Linguagem de Programação II/AP2/questão_09.py
@@ -3,3 +3,15 @@
 # Escreva um programa que gere um dicionário, em que cada chave seja um caractere, e seu
 # valor seja o número deste caractere encontrado em uma frase lida.
 # Exemplo: O rato -> {"O":1, "r":’, "a":1, "t":1, "o": 1}
+
+d = {}
+frase = input("Digite uma frase: ")
+for letra in frase:
+    if letra in d:
+        # se a letra já estiver no dicionário, é somado +1
+        d[letra] = d[letra] + 1
+    else:
+        # se não houver atribui a letra e atribui 1
+        d[letra] = 1
+
+print(d)
